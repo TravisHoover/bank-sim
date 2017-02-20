866f5648cbf3241a1d050498ff5c288c5da3f034
Travis Hoover <thoover@smartvue.com>
2017-02-19 19:59:09 -0600

    added function to determine customer file to open

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <pthread.h>
+
+
+int numFiles = 4;
+
+struct Account {
+    int number;
+    float balance;
+};
 
 int main() {
-    printf("Hello, World!\n");
+
+    pthread_t files[numFiles];
+    struct Account account[numFiles];
+    FILE *pFile;
+
+    for (int i = 0; i < numFiles; i++) {
+
+        // Determine customer file to be opened
+        char fileName[10];
+        snprintf(fileName, sizeof(fileName), "cust%d.dat", i);
+
+        printf("Opening file: %s\n", fileName);
+        pFile = fopen(fileName, "r");
+
+        fscanf (pFile, "%d", &account[i].number);
+        printf("Account number: %08d\n", account[i].number);
+
+        fscanf (pFile, "%f", &account[i].balance);
+        printf("Account balance: $%.2f\n\n", account[i].balance);
+    }
+
     return 0;
 }
