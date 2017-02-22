c0dbbe6922162d2abb086e6fadfedad7bd792523
Travis Hoover <thoover@smartvue.com>
2017-02-22 10:30:06 -0600

    Corrected file reads for ATM threads, added join

--- main.c
+++ main.c
@@ -3,72 +3,75 @@
 
 
 int numFiles = 4;
-int numATMs = 3;
+const int numATMs = 3;
+int thread_ids[numATMs] = {0, 1, 2};
 
 struct Account {
     int number;
     float balance;
+};
+
+void *transaction(void * arg) {
+    int accNumber;
     char tranType;
     float tranAmount;
     int waitTime;
-};
-
-void *transaction(void *arg) {
-    struct Account *input = arg;
-    int accNumber = input->number;
-    char tranType = input->tranType;
-    float tranAmount = input->tranAmount;
-    int waitTime = input->waitTime;
 
     FILE *transaction;
 
     transaction = fopen("atm0.dat", "r");
-    fscanf(transaction, "%d", &accNumber);
-    fscanf(transaction, "%c", &tranType);
-    fscanf(transaction, "%f", &tranAmount);
-    fscanf(transaction, "%d", &waitTime);
-    printf("\nAccount: %08d\n", accNumber);
-    printf("Transaction type: %c\n", tranType);
-    printf("Transaction amount: %.2f\n", tranAmount);
-    printf("Wait time: %d\n\n", waitTime);
+    for (;;) {
+
+        fscanf(transaction, "%d %c %f %d", &accNumber, &tranType, & tranAmount, &waitTime);
+
+        if (feof(transaction)) break;
+
+        printf("\nAccount: %08d\n", accNumber);
+        printf("Transaction type: %c\n", tranType);
+        printf("Transaction amount: %.2f\n", tranAmount);
+        printf("Wait time: %d\n", waitTime);
+    }
 
     return NULL;
 }
 
 int main() {
 
-    int i, j;
+    int i, j, k;
     struct Account account[numFiles];
     FILE *pFile;
-    //pthread_t thread[numATMs];
+    pthread_t thread[numATMs];
 
+    /*** Read in customer accounts ***/
     for (i = 0; i < numFiles; i++) {
-
         // Determine customer file to be opened
         char fileName[10];
         snprintf(fileName, sizeof(fileName), "cust%d.dat", i);
 
         printf("Opening file: %s\n", fileName);
         pFile = fopen(fileName, "r");
-        fscanf(pFile, "%d", &account[i].number);
+        fscanf(pFile, "%d %f", &account[i].number, &account[i].balance);
         printf("Account number: %08d\n", account[i].number);
-        fscanf(pFile, "%f", &account[i].balance);
         printf("Account balance: $%.2f\n\n", account[i].balance);
     }
-
+printf("****************************************************");
+    /*** Reopen the files for writing ***/
     /* Not sure why this is needed yet */
     //pFile = fopen(fileName, "w");
     //fprintf(pFile, "%f", account[i].balance);
 
     /*** ATM implementation ***/
     for (j = 0; j < numATMs; j++) {
-        //pthread_create(&thread[j], NULL, &transaction, &account[j]);
+        pthread_create(&thread[j], NULL, transaction, &thread_ids[j]);
         //pass one of the atm.dat files into this thread
         //pthread_join(thread[j], NULL);
     }
 
-    /*** main.c work queue ***/
+    for (k = 0; k < numATMs; k++) {
+        pthread_join(thread[k], NULL);
+    }
 
+    /*** main.c work queue ***/
 
 
     return 0;
