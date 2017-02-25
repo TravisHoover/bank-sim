3372136556206f8c2e7fdd168a0890a0ed0025cf
Travis Hoover <thoover@smartvue.com>
2017-02-25 13:33:34 -0600

    Started main’s queue implementation

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
+/*
+ * Author: Travis Hoover
+ * Date: March 1, 2017
+ * Course: CSCI 4300
+ * Professor: Dr. Pettey
+ *
+ * This program will simulate a bank, practicing work with concurrent programming
+ * by treating each pthread as an individual ATM.
+ */
+
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
 
 const int numFiles = 4;
 const int numATMs = 3;
 int thread_ids[3] = {0, 1, 2};
 int done = 0;
+int queueCount = 0;
 
 struct Account {
     int number;
     float balance;
 };
+
+struct Queue {
+    int atmNum;
+    int accNum;
+    char transactionType;
+    float transactionAmount;
+    float balance;
+};
+
 struct Account account[4];
+struct Queue queue[3];
 
 void *transaction(void * arg) {
     int accNumber;
@@ -37,11 +57,6 @@ void *transaction(void * arg) {
 
         if (feof(transaction)) break;
 
-        /*printf("\nAccount: %08d\n", accNumber);
-        printf("Transaction type: %c\n", tranType);
-        printf("Transaction amount: %.2f\n", tranAmount);
-        printf("Wait time: %d\n", waitTime);*/
-
         //update account
         for (t = 0; t < numFiles; t++) {            //scan all cust.dat files to find account
             if (accNumber == account[t].number) {
@@ -58,9 +73,16 @@ void *transaction(void * arg) {
 
                 //print overdrawn message if necessary
                 if (account[t].balance < 0) {
-                    printf("Warning: Account %08d has overdrafted. There will be a penalty of $10\n", accNumber);
+                    printf("Warning: Account %08d has become overdraft. There will be a penalty of $10\n", accNumber);
                     account[t].balance -= 10;
                 }
+
+                queue[queueCount].atmNum = threadID;
+                queue[queueCount].accNum = accNumber;
+                queue[queueCount].transactionType = tranType;
+                queue[queueCount].transactionAmount = tranAmount;
+                queue[queueCount].balance = account[t].balance;
+                queueCount++;
             }
             //put the transaction into the main program's work queue
 
@@ -77,6 +99,7 @@ void *transaction(void * arg) {
 int main() {
 
     int i, j, k;
+    queueCount = 1;
 
     FILE *pFile;
     pthread_t thread[numATMs];
@@ -90,37 +113,41 @@ int main() {
         printf("Opening file: %s\n", fileName);
         pFile = fopen(fileName, "r");
         fscanf(pFile, "%d %f", &account[i].number, &account[i].balance);
-        //printf("Account number: %08d\n", account[i].number);
-        //printf("Account balance: $%.2f\n\n", account[i].balance);
     }
-//printf("********** Transactions **********");
-    /*** Reopen the files for writing ***/
-    /* Not sure why this is needed yet */
-    //pFile = fopen(fileName, "w");
-    //fprintf(pFile, "%f", account[i].balance);
 
     /*** ATM implementation ***/
     for (j = 0; j < numATMs; j++) {
         pthread_create(&thread[j], NULL, transaction, &thread_ids[j]);
-        //pass one of the atm.dat files into this thread
+    }
+
+    /*** main.c work queue ***/
+
+    if (queueCount == 3) {
+        while (queueCount > 0) {
+
+            //print transactions to the appropriate customer file
+            char fileName[10];
+            snprintf(fileName, sizeof(fileName), "cust%d.dat", queue->accNum);
+            pFile = fopen(fileName, "w");
+
+            fprintf(pFile, "%d %c %f %f\n", queue->atmNum, queue->transactionType, queue->transactionAmount,
+                    queue->balance);
+            queueCount--;
+        }
     }
 
     for (k = 0; k < numATMs; k++) {
         pthread_join(thread[k], NULL);
     }
 
-    /*** main.c work queue ***/
-
     if (done == numATMs) {
         printf("\n/***** Final Balances *****/\n");
         for (i = 0; i < numFiles; i++) {
-            printf("Account number: %08d\n", account[i].number);
-            printf("Account balance: $%.2f\n\n", account[i].balance);
+            printf("The ending balance for %08d is $%.2f\n\n", account[i].number, account[i].balance);
         }
     } else {
         printf("\nError: one of the threads did not complete successfully\n");
     }
-
 
     return 0;
 }
